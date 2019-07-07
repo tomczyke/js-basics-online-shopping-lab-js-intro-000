c6c3f7db424e1fd3cc417a40b47b136372c20167
Evan Tomczyk <tomczykej@gmail.com>
2019-07-07 19:33:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function addToCart(item) {
   var price = Math.floor(Math.random() * 100);
   cart.push({[item]: price})
   console.log(`${item} has been added to your cart.`);
-  return cart;
+  return cart
 }
 
 function viewCart() {
