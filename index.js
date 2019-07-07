7e9ce8d2c578444ddfde58669312e32b1e47bd35
Evan Tomczyk <tomczykej@gmail.com>
2019-07-07 20:15:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,23 @@ function addToCart(item) {
 }
 
 function viewCart() {
+  var statementArray = [];
+  if (cart.length === 0) {
+    return "Your shopping cart is empty.";
+  } else {
+    for (var i = 0; i < cart.length; i++) {
+      return `in your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}.`;
+    }
+  }
+
+}
+
+
+
+
+
+
+/*function viewCart() {
   // write your code here
   var statementArray = [];
   if (cart.length === 0) {
@@ -27,8 +44,8 @@ function viewCart() {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
   } else if (cart.length === 2) {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
-  }
-}
+  } else
+}*/
 
 function total() {
   // write your code here
